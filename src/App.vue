593eed3d3f6ea4e161dcb9e945fa5ff2e95a1229
Teo <43215755+teokigi@users.noreply.github.com>
2020-04-25 23:53:48 +0100

    finally got the api working and retrieving data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template lang="html">
   <body id="app">
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <h1>Hello Eorzea</h1>
+    <div class="PaneBody">
+        <div class="leftPane">
+          
+        </div>
+        <div class="mainPane">
+
+        </div>
+  </div>
   </body>
 </template>
 
 <script>
 export default {
+  name:'App',
+  data(){
+    return{
+      ffxivapi: null
+    }
+  },
+  mounted(){
+    this.getffxivapi()
+  },
+  methods:{
+    getffxivapi(){
+      console.log('')
+      fetch("https://staging.xivapi.com/freecompany/9229283011365729585?data=FCM")
+    .then(res => res.json())
+    .then(resstuff => this.ffxivapi=resstuff)
+    }
+  }
 }
 </script>
 
@@ -32,3 +55,14 @@ div{
   border-width:thin;
 }
 </style>
+<!-- MVP
+The application should display data from an API request.
+The application should have a clear separation of concerns (multiple components)
+Take input from the user to update the page. You could update the page by filtering or manipulating the data on user interaction, or you might make further API requests to load more data that is then displayed.
+Extensions
+Looking into a library to visual the data.
+
+Leaflet is an open-source library for rendering maps
+Google Charts is a library for rendering charts and graphs
+Canvas is a drawing surface for JavaScript.
+You will need to use the library’s documentation to integrate it into your application. -->
